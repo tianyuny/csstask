8425fac5605ae8f9f432aeab705c279ab56a0884
yijingjun <1698233847@qq.com>
2017-12-17 17:32:49 +0800

    js-one

--- me/3Dheart/js/aixin.js
+++ me/3Dheart/js/aixin.js
@@ -6,7 +6,6 @@ window.onload = function() {
         //heart.className = "heart";
         //heart.style.transform = "rotatey("+i*10+"deg) rotatez(44.5deg) translatex(19.6vw)";
         //myHeart.appendChild(heart);
-
         var child = document.createElement('div');
         child.style.transform = "rotatey("+i*10+"deg) rotatez(44.5deg) translatex(14.8vw)";
         heartChild.appendChild(child);
