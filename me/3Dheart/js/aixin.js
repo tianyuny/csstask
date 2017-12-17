9c235e1af7c517dcc9075e712af05c75b650ee99
yijingjun <1698233847@qq.com>
2017-12-17 15:08:00 +0800

    js-one

--- me/3Dheart/js/aixin.js
+++ me/3Dheart/js/aixin.js
@@ -1,16 +1,12 @@
 window.onload = function() {
-    var myHeart = document.getElementById('heart');
+    //var myHeart = document.getElementById('heart');
     var heartChild = document.getElementById('heartchild');
     for (var i = 0; i < 36; i++) {
-        var heart = document.createElement('div');
+        //var heart = document.createElement('div');
+        //heart.className = "heart";
+        //heart.style.transform = "rotatey("+i*10+"deg) rotatez(44.5deg) translatex(19.6vw)";
+        //myHeart.appendChild(heart);
 
-        heart.className = "heart";
-        heart.style.transform = "rotatey("+i*10+"deg) rotatez(44.5deg) translatex(19.6vw)";
-
-        myHeart.appendChild(heart);
-        if (i%2 === 0) {
-
-        }
         var child = document.createElement('div');
         child.style.transform = "rotatey("+i*10+"deg) rotatez(44.5deg) translatex(14.8vw)";
         heartChild.appendChild(child);
