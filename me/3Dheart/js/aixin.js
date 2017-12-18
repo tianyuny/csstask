eac9c066c59a29345ab936c06ab96acb8c1f6ae2
yijingjun <1698233847@qq.com>
2017-12-18 16:25:25 +0800

    js-one

--- me/3Dheart/js/aixin.js
+++ me/3Dheart/js/aixin.js
@@ -1,13 +1,15 @@
 var body = document.getElementsByTagName('body');
-var a = "10% 40%";
+var a = Math.floor(Math.random()*100) + "% " + (Math.floor(Math.random()*90) + 10) + "%";
 var b = "150px";
-for (var x = 0; x < 18; x++) {
+for (var x = 0; x < 50; x++) {
     a = a + "," + Math.floor(Math.random()*100) + "% " + (Math.floor(Math.random()*90) + 10) + "%";
-    if (x % 2 === 0) {
+    if (x % 4 === 0) {
         b = b + "," + Math.floor(Math.random() * 100) + "px";
         //alert(background);
-    } else {
+    } else if (x % 2) {
         b = b + "," + (Math.floor(Math.random() * 50) + 5) + "px";
+    } else {
+        b = b + "," + (Math.floor(Math.random() * 10) + 5) + "px";
     }
 }
 body[0].style.backgroundPosition = a;
@@ -15,14 +17,10 @@ body[0].style.backgroundSize = b;
 window.onload = function() {
     var heartChild = document.getElementById('heartchild');
     var header = document.getElementById('header');
-    var body = document.getElementsByTagName('body');
-    var a = "10% 40%";
-    var b = "150px";
     header.className = "header";
     for (var i = 0; i < 36; i++) {
         var child = document.createElement('div');
         child.style.transform = "rotatey(" + i * 10 + "deg) rotatez(44.5deg) translatex(14.8vw)";
         heartChild.appendChild(child);
-        a = a + "," + Math.floor(Math.random()*100) + "% " + (Math.floor(Math.random()*90) + 10) + "%";
     }
 };
