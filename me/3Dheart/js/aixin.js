64d1d788b1267dd5bad3322d12d115db8d30d2fe
yijingjun <1698233847@qq.com>
2017-12-18 16:06:59 +0800

    js-one

--- me/3Dheart/js/aixin.js
+++ me/3Dheart/js/aixin.js
@@ -1,3 +1,17 @@
+var body = document.getElementsByTagName('body');
+var a = "10% 40%";
+var b = "150px";
+for (var x = 0; x < 18; x++) {
+    a = a + "," + Math.floor(Math.random()*100) + "% " + (Math.floor(Math.random()*90) + 10) + "%";
+    if (x % 2 === 0) {
+        b = b + "," + Math.floor(Math.random() * 100) + "px";
+        //alert(background);
+    } else {
+        b = b + "," + (Math.floor(Math.random() * 50) + 5) + "px";
+    }
+}
+body[0].style.backgroundPosition = a;
+body[0].style.backgroundSize = b;
 window.onload = function() {
     var heartChild = document.getElementById('heartchild');
     var header = document.getElementById('header');
@@ -10,13 +24,5 @@ window.onload = function() {
         child.style.transform = "rotatey(" + i * 10 + "deg) rotatez(44.5deg) translatex(14.8vw)";
         heartChild.appendChild(child);
         a = a + "," + Math.floor(Math.random()*100) + "% " + (Math.floor(Math.random()*90) + 10) + "%";
-        if (i%4 === 0) {
-            b = b + "," + Math.floor(Math.random()*100) + "px";
-            //alert(background);
-        } else if (i%2 === 0){
-            b = b + "," + (Math.floor(Math.random()*50) + 5) + "px";
-        }
     }
-    body[0].style.backgroundPosition = a;
-    body[0].style.backgroundSize = b;
 };
